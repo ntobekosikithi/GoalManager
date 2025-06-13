35b26be45439df1d04bce1b30c2f619e5e900c4c
Ntobeko Sikithi <ntobekosikithi@gmail.com.com>
2025-06-13 20:25:47 +0200

    Initial commit

--- Package.swift
+++ Package.swift
@@ -11,11 +11,15 @@ let package = Package(
             name: "GoalManager",
             targets: ["GoalManager"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ntobekosikithi/Utilities.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "GoalManager"),
+            name: "GoalManager",
+            dependencies: ["Utilities"]),
         .testTarget(
             name: "GoalManagerTests",
             dependencies: ["GoalManager"]
